4964b86447d2ba0d9994f1d63028953fe7a759af
Andres Javier Lopez <ajavier.lopez@gmail.com>
2022-02-11 18:12:39 -0600

    Add support for postgresql database

---
+++ sigil/store/adapters/psql.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.future import select as future_select
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.sql import Select
+
+from sigil import settings
+
+
+def create_connection_string(user, password, host, port, db):
+    return f'postgresql+asyncpg://{user}:{password}@{host}:{port}/{db}'
+
+
+engine = create_async_engine(create_connection_string(
+    settings.DB_USER,
+    settings.DB_PASSWORD,
+    settings.DB_HOST,
+    settings.DB_PORT,
+    settings.DB_NAME,
+))
+
+
+Session: AsyncSession = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+Base = declarative_base()
+
+
+def select(*entities) -> Select:
+    """Wrapper for future select to improve typing"""
+    return future_select(*entities)
