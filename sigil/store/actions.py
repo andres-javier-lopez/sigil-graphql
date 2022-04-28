d049fd6f694e933d1658eb4c31bee3bd399f380b
Andres Javier Lopez <ajavier.lopez@gmail.com>
2022-04-28 08:17:58 -0600

    Improve names for seed commands

--- sigil/store/actions.py
+++ sigil/store/actions.py
@@ -1,3 +1,3 @@
-from sigil.store.adapters.psql.seed import init_storage as init
-from sigil.store.adapters.psql.seed import upgrade_storage as upgrade
+from sigil.store.adapters.psql.commands import init_storage as init
+from sigil.store.adapters.psql.commands import upgrade_storage as upgrade
 from sigil.store.seed import seed
--- sigil/store/seed.py
+++ sigil/store/seed.py
@@ -1,8 +1,8 @@
-from sigil.store.adapters.psql.seed import seed_stores
+from sigil.store.adapters.psql.commands import include_storages
 from sigil.store.domain.campaign import seed_campaigns
 from sigil.store.interfaces import CampaignStore
 
 
-@seed_stores
+@include_storages
 async def seed(*, stores: dict = None):
     await seed_campaigns(stores[CampaignStore.__name__], number=5)
