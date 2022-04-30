073c5a99bc70b5b8eaebb8cdf81f86e7eb79369b
Andres Javier Lopez <ajavier.lopez@gmail.com>
2022-04-30 12:14:59 -0600

    Add database update to Heroku script

--- scripts/launch.heroku.sh
+++ scripts/launch.heroku.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 poetry run python --version
+poetry run python -m sigil storage upgrade
 poetry run python -m uvicorn sigil.app:app --host 0.0.0.0 --port $PORT --log-level warning
