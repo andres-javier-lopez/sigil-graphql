830e11ff001cc920f529d1b82f89cd8ba99a4c05
Andres Javier Lopez <ajavier.lopez@gmail.com>
2022-05-27 16:48:42 -0600

    Add Hub entity

--- sigil/domain/entities.py
+++ sigil/domain/entities.py
@@ -1,5 +1,6 @@
 from sigil.domain.campaign.entities.base import Campaign as BaseCampaign
 from sigil.domain.campaign.entities.base import Party, PlayerCharacter
+from sigil.domain.town.entities.base import Hub
 from sigil.domain.town.entities.mixins import CampaignTownMixin
 
 
--- sigil/domain/town/entities/base.py
+++ sigil/domain/town/entities/base.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from uuid import uuid4
 
 from pydantic import UUID4, BaseModel, Field
@@ -8,3 +9,10 @@ from sigil.domain.campaign.entities.base import Campaign
 class Hub(BaseModel):
     uuid: UUID4 = Field(default_factory=uuid4)
     campaign: Campaign
+    name: str
+    description: Optional[str]
+    notes: Optional[str]
+
+    @property
+    def campaign_id(self):
+        return self.campaign.uuid
