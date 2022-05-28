d00ab9a8726152b4adfa3d77339bb9c7d24d9eeb
Andres Javier Lopez <ajavier.lopez@gmail.com>
2022-05-27 20:24:55 -0600

    Use new manager for seeding

--- sigil/storage/adapters/psql/tests/test_psql_seed.py
+++ sigil/storage/adapters/psql/tests/test_psql_seed.py
@@ -2,7 +2,6 @@ from unittest import mock
 
 import pytest
 
-from sigil.storage.adapters.psql.commands import include_storages
 from sigil.storage.adapters.psql.storages import CampaignStorage
 from sigil.storage.seed import seed
 
@@ -15,12 +14,10 @@ async def test_database_seed(db_session):
         assert len(campaigns) == 0
 
     with mock.patch(
-        "sigil.storage.adapters.psql.commands.async_session",
+        "sigil.storage.interfaces.async_session",
         return_value=db_session,
     ):
-        # redecorate the function to avoid any misconfiguration
-        psql_seed = include_storages(seed.__wrapped__)
-        await psql_seed()
+        await seed()
 
     campaigns = await CampaignStorage(db_session).list()
     assert len(campaigns) > 0
--- sigil/storage/seed.py
+++ sigil/storage/seed.py
@@ -1,24 +1,18 @@
-from sigil.storage.adapters.psql.commands import include_storages
 from sigil.storage.domain.campaign import (
     seed_campaigns,
     seed_party,
     seed_player_characters,
 )
 from sigil.storage.domain.town import seed_hubs
-from sigil.storage.interfaces import (
-    CampaignStorage,
-    HubStorage,
-    PartyStorage,
-    PlayerCharacterStorage,
-)
+from sigil.storage.interfaces import StorageManager
 
 
-@include_storages
-async def seed(*, storages: dict = None):
-    campaigns = await seed_campaigns(storages[CampaignStorage.__name__], number=3)
-    for campaign in campaigns:
-        player_characters = await seed_player_characters(
-            storages[PlayerCharacterStorage.__name__], campaign, number=4
-        )
-        await seed_party(storages[PartyStorage.__name__], campaign, player_characters)
-        await seed_hubs(storages[HubStorage.__name__], campaign)
+async def seed():
+    async with StorageManager.start() as storages:
+        campaigns = await seed_campaigns(storages.campaign_storage, number=3)
+        for campaign in campaigns:
+            player_characters = await seed_player_characters(
+                storages.player_character_storage, campaign, number=4
+            )
+            await seed_party(storages.party_storage, campaign, player_characters)
+            await seed_hubs(storages.hub_storage, campaign)
