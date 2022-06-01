41be5b00bb523c028c663ae77bcb47da5e0c9f96
Andres Javier Lopez <ajavier.lopez@gmail.com>
2022-05-31 18:31:39 -0600

    Add base manager for storage

--- sigil/storage/adapters/psql/manager.py
+++ sigil/storage/adapters/psql/manager.py
@@ -1,3 +1,4 @@
+import logging
 from contextlib import asynccontextmanager
 
 from sigil.storage.adapters.psql import async_session
@@ -7,43 +8,24 @@ from sigil.storage.adapters.psql.storages import (
     PartyStorage,
     PlayerCharacterStorage,
 )
+from sigil.storage.base import BaseManager
+
+logger = logging.getLogger(__name__)
 
 
-class StorageManager:
+class StorageManager(BaseManager):
     def __init__(self, session):
         self.session = session
-        self._campaign_storage = None
-        self._hub_storage = None
-        self._party_storage = None
-        self._player_character_storage = None
 
-    @property
-    def campaign_storage(self):
-        if self._campaign_storage is None:
-            self._campaign_storage = CampaignStorage(self.session)
+        self._storage_classes = {
+            "campaign_storage": CampaignStorage,
+            "hub_storage": HubStorage,
+            "party_storage": PartyStorage,
+            "player_character_storage": PlayerCharacterStorage,
+        }
 
-        return self._campaign_storage
-
-    @property
-    def hub_storage(self):
-        if self._hub_storage is None:
-            self._hub_storage = HubStorage(self.session)
-
-        return self._hub_storage
-
-    @property
-    def party_storage(self):
-        if self._party_storage is None:
-            self._party_storage = PartyStorage(self.session)
-
-        return self._party_storage
-
-    @property
-    def player_character_storage(self):
-        if self._player_character_storage is None:
-            self._player_character_storage = PlayerCharacterStorage(self.session)
-
-        return self._player_character_storage
+    def _init_storage(self, StorageClass):
+        return StorageClass(self.session)
 
     @classmethod
     @asynccontextmanager
@@ -53,6 +35,7 @@ class StorageManager:
                 try:
                     yield cls(session)
                 except Exception:
+                    logger.exception("rolling back session")
                     await session.rollback()
                 else:
                     await session.commit()
--- sigil/storage/base.py
+++ sigil/storage/base.py
@@ -1,5 +1,6 @@
 """Base classes for storage adapters"""
 import abc
+from contextlib import asynccontextmanager
 from typing import List, Optional
 
 from pydantic import UUID4, BaseModel
@@ -25,3 +26,26 @@ class BaseStorage(abc.ABC):
     @abc.abstractmethod
     async def delete(self, uuid: UUID4):
         raise NotImplementedError
+
+
+class BaseManager:
+    _storages = {}
+
+    _storage_classes = {}
+
+    def _init_storage(self, StorageClass):
+        return StorageClass()
+
+    def __getattr__(self, name: str):
+        if name not in self._storage_classes:
+            raise AttributeError(f"no storage called {name}")
+
+        if name not in self._storages or self._storages[name] is None:
+            self._storages[name] = self._init_storage(self._storage_classes[name])
+
+        return self._storages[name]
+
+    @classmethod
+    @asynccontextmanager
+    async def start(cls):
+        yield cls()
